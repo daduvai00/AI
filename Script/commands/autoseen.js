1f20a0dd033b056d31857f0d9b90fccc2adfc338
Tamim Khan <tinverkhan0@gmail.com>
2025-07-13 19:40:13 +0600

    Update autoseen.js

--- Script/commands/autoseen.js
+++ Script/commands/autoseen.js
@@ -1,7 +1,7 @@
-
 const fs = require('fs-extra');
 const pathFile = __dirname + '/cache/autoseen.txt';
 
+// ফাইল চেক এবং ডিফল্ট মান সেট করা
 if (!fs.existsSync(pathFile)) fs.writeFileSync(pathFile, 'true');
 
 module.exports = {
@@ -16,9 +16,9 @@ module.exports = {
 };
 
 module.exports.handleEvent = async ({ api, event }) => {
-    const pathFile = __dirname + '/cache/autoseen.txt';
-    const autoSeenStatus = 'true'; // Always true for auto seen
-    
+    // সবসময় সিন করার জন্য এখানে ফাইল চেক করা হচ্ছে
+    const autoSeenStatus = fs.readFileSync(pathFile, 'utf-8').trim();
+
     if (autoSeenStatus === 'true') {
         api.markAsReadAll(() => {});
     }
@@ -29,16 +29,15 @@ module.exports.run = async ({ api, event, args }) => {
     try {
         if (args[0] === 'on') {
             fs.writeFileSync(pathFile, 'true');
-            api.sendMessage('Auto seen turn on successfully.', event.threadID, event.messageID);
+            api.sendMessage('Auto seen turned on successfully.', event.threadID, event.messageID);
         } else if (args[0] === 'off') {
             fs.writeFileSync(pathFile, 'false');
-            api.sendMessage('Auto seen turn off successfully.', event.threadID, event.messageID);
+            api.sendMessage('Auto seen turned off successfully.', event.threadID, event.messageID);
         } else {
-            api.sendMessage('Wrong format\nUse: on/off', event.threadID, event.messageID);
+            api.sendMessage('Incorrect format\nUse: on/off', event.threadID, event.messageID);
         }
     } catch (error) {
         console.log(error);
     }
 };
 ```
-
