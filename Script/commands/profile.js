08493d67b9e380ee649c2e3a8600ef2254897aa4
Tamim Khan <tinverkhan0@gmail.com>
2025-07-15 19:28:06 +0600

    Create profile.js

---
+++ Script/commands/profile.js
@@ -0,0 +1,17 @@
+this.config = {
+ name: "profile",
+ version: "1.0.0",
+ usePrefix: true,
+ hasPermssion: 0,
+ credits: "TOHIDUL",
+ description: "",
+ commandCategory: "admin",
+ usages: "",
+ cooldowns: 5,
+ };
+ 
+ this.run = async ({ api: { shareContact }, event: { threadID, messageReply, senderID, mentions, type }, args }) => {
+ let id = Object.keys(mentions).length > 0 ? Object.keys(mentions)[0].replace(/\&mibextid=ZbWKwL/g, '') : args[0] !== undefined ? isNaN(args[0]) ? await global.utils.getUID(args[0]) : args[0] : senderID;
+ if (type === "message_reply") id = messageReply.senderID;
+ shareContact("", id, threadID);
+ };
